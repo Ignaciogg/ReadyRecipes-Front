c4992334361e96bbcd5979e0f72ef755c79582a4
Samuel Fernando Méndez Saldías <72949404+sfms97@users.noreply.github.com>
2022-11-23 12:18:07 +0100

    Textos desconocido def

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import YoutubeDownloader
 import os
 
-'''with open('otros.txt','r') as file:
+with open('otros.txt','r') as file:
     for linea in file:
-        YoutubeDownloader.descargarVideo(linea)'''
+        YoutubeDownloader.descargarVideo(linea)
 
 #YoutubeDownloader.descargarVideo('https://www.youtube.com/watch?v=U1JXE7o6Z8g')
 contenido = os.listdir('./textos/otros')
