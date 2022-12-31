7c3f1532ec91e0a489b66d66646d07fe9c8ea603
Ignacio Gil <nachogilgarzon@gmail.com>
2022-12-31 14:33:03 +0100

    abrir explorador y seleccionar ruta

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -1,7 +1,13 @@
-# pip install tkinter
+''' 	
+	pip install tkinter
+ 	python -m pip install ttkbootstrap
+'''
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog
+import os
 import pathlib
+
+ROOT_DIR = os.path.abspath(os.curdir)
 
 class Ventana(Frame):
 	def __init__(self, master, *args):
@@ -10,25 +16,24 @@ class Ventana(Frame):
 		# Declaración de variables
 		self.menu = True
 		self.color = True
-		self.ruta_aperitivos = StringVar()
-		self.ruta_aperitivos.set(".\ReadyRecipes\Textos\Aperitivos")
-		self.ruta_carne = StringVar()
-		self.ruta_carne.set(".\ReadyRecipes\Textos\Carnes")
-		self.ruta_pasta = StringVar()
-		self.ruta_pasta.set(".\ReadyRecipes\Textos\Pastas")
-		self.ruta_pescado = StringVar()
-		self.ruta_pescado.set(".\ReadyRecipes\Textos\Pescados")
-		self.ruta_verdura = StringVar()
-		self.ruta_verdura.set(".\ReadyRecipes\Textos\Verduras")
-		self.ruta_otros = StringVar()
-		self.ruta_otros.set(".\ReadyRecipes\Textos\otros")
-		self.ruta_modelo = StringVar()
-		self.ruta_modelo.set(".\ReadyRecipes\Textos\modelo")
-		self.ruta_guardar_modelo = StringVar()
-		self.ruta_guardar_modelo.set(".\ReadyRecipes\Textos\modelo\modelo1.txt")
-		self.seleccion = IntVar()
-		self.seleccion.set(1)
-		self.aperitivos, self.carnes, self.pastas, self.pescados, self.verduras, self.total = 0,0,0,0,0,0
+
+		self.ruta_aperitivos, self.ruta_carne, self.ruta_pasta, self.ruta_pescado, self.ruta_verdura, self.ruta_otros, self.ruta_modelo, self.ruta_guardar_modelo = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar()
+		self.ruta_aperitivos.set("./ReadyRecipes/Textos/Aperitivos")
+		self.ruta_carne.set("./ReadyRecipes/Textos/Carnes")
+		self.ruta_pasta.set("./ReadyRecipes/Textos/Pastas")
+		self.ruta_pescado.set("./ReadyRecipes/Textos/Pescados")
+		self.ruta_verdura.set("C:/Users/Nacho/git//ReadyRecipes/Textos/Verduras")
+		self.ruta_otros.set("./ReadyRecipes/Textos/otros")
+		self.ruta_modelo.set("./ReadyRecipes/Textos/modelo")
+		self.ruta_guardar_modelo.set("./ReadyRecipes/Textos/modelo/modelo1.txt")
+		self.seleccion = StringVar()
+
+		self.aperitivos, self.carnes, self.pastas, self.pescados, self.verduras, self.total = IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()
+		self.aperitivos.set(contarTextos(self.ruta_aperitivos.get()))
+		self.carnes.set(contarTextos(self.ruta_carne.get()))
+		self.pastas.set(contarTextos(self.ruta_pasta.get()))
+		self.pescados.set(contarTextos(self.ruta_pescado.get()))
+		self.verduras.set(contarTextos(self.ruta_verdura.get()))
 
 		# Settings frame principal
 		self.frame_top = Frame(self.master, bg='#061a2b', height = 50)
@@ -60,13 +65,20 @@ class Ventana(Frame):
 		self.paginas.select([self.frame_tres])	
 		self.frame_tres.columnconfigure(0, weight=1)
 		self.frame_tres.columnconfigure(1, weight=1)
+	
+	def seleccionaAlgoritmo(self, seleccion):
+		self.seleccion.set(seleccion)
+
+	def abrirExplorador(self, ruta):
+		ruta.set(filedialog.askdirectory())
+		print(self.ruta_aperitivos.get())
 
 	def nav(self):
 		self.imagen_entrenamiento = PhotoImage(file ='./interfaz/entrenamiento.png')
 		self.imagen_clasificacion = PhotoImage(file ='./interfaz/clasifiacion.png')
 		self.imagen_webscraping = PhotoImage(file ='./interfaz/webscraping.png')
 		self.imagen_home = PhotoImage(file ='./interfaz/home.png')
-		self.logo = PhotoImage(file ='./interfaz/readyrecipes_logo.png')
+		self.logo = PhotoImage(file ='./interfaz/logo.png')
 
 		self.paginas = ttk.Notebook(self.frame_principal, style= 'TNotebook')
 		self.paginas.grid(column=0, row=0, sticky='nsew')
@@ -80,13 +92,13 @@ class Ventana(Frame):
 		self.paginas.add(self.frame_tres, image = self.imagen_webscraping)
 
 		# Inicio
-		Label(self.frame_top,text='Bienvenido a Ready Recipes', bg='#061a2b', fg='#4077a6', font=('Arial', 25, 'bold')).pack(expand=1, pady=12,)
-		Label(self.frame_inicio ,image= self.logo, bg='white').pack(expand=1, pady=0)
+		Label(self.frame_top,text='Bienvenido a Ready Recipes', bg='#061a2b', fg='#4077a6', font=('Arial', 25, 'bold')).pack(expand=1, pady=12)
+		Label(self.frame_inicio, image= self.logo, bg='white').pack(expand=1, pady=0)
 		Label(self.frame_inicio, text= 'Para empezar, seleccione el segundo icono (entrenamiento) del navegador', bg='white', fg= 'black', font= ('Arial', 15)).pack(expand=1)
 
 		# Página 1 - Entrenamiento
 		#1.1 - Selección de textos
-		Label(self.frame_uno, text='ENTRENAMIENTO', bg='white', fg= 'black', font= ('Arial', 15, 'bold')).grid(column=1, row=0, pady=12)
+		Label(self.frame_uno, text='ENTRENAMIENTO', bg='white', fg= 'black', font= ('Arial', 15, 'bold')).grid(column=1, row=0, pady=20)
 		Label(self.frame_uno, width=15, text='Textos Aperitivos', bg='white', fg= 'black', font=('Arial', 13)).grid(column=0, row=1, pady=12, padx=100)
 		Label(self.frame_uno, width=15, text='Textos Carne', bg='white', fg= 'black', font=('Arial', 13)).grid(column=0, row=2, pady=12, padx=100)
 		Label(self.frame_uno, width=15, text='Textos Pasta', bg='white', fg= 'black', font=('Arial', 13)).grid(column=0 ,row=3, pady=12, padx=100)
@@ -99,27 +111,25 @@ class Ventana(Frame):
 		Entry(self.frame_uno, state= "disabled", width=80, text=self.ruta_pescado, textvariable=self.ruta_pescado, font=('Arial', 10), highlightbackground = "#061a2b", highlightthickness=3).grid(column=1,row=4, padx=140)
 		Entry(self.frame_uno, state= "disabled", width=80, text=self.ruta_verdura, textvariable=self.ruta_verdura, font=('Arial', 10), highlightbackground = "#061a2b", highlightthickness=3).grid(column=1,row=5, padx=140)
 
-		Button(self.frame_uno, width=12, text='Abrir').grid(column=2, row=1, padx=50)
-		Button(self.frame_uno, width=12, text='Abrir').grid(column=2, row=2, padx=50)
-		Button(self.frame_uno, width=12, text='Abrir').grid(column=2, row=3, padx=50)
-		Button(self.frame_uno, width=12, text='Abrir').grid(column=2, row=4, padx=50)
-		Button(self.frame_uno, width=12, text='Abrir').grid(column=2, row=5, padx=50)
+		Button(self.frame_uno, width=12, text='Cambiar', command= lambda : self.abrirExplorador(self.ruta_aperitivos)).grid(column=2, row=1, padx=50)
+		Button(self.frame_uno, width=12, text='Cambiar', command= lambda : self.abrirExplorador(self.ruta_carne)).grid(column=2, row=2, padx=50)
+		Button(self.frame_uno, width=12, text='Cambiar', command= lambda : self.abrirExplorador(self.ruta_pasta)).grid(column=2, row=3, padx=50)
+		Button(self.frame_uno, width=12, text='Cambiar', command= lambda : self.abrirExplorador(self.ruta_pescado)).grid(column=2, row=4, padx=50)
+		Button(self.frame_uno, width=12, text='Cambiar', command= lambda : self.abrirExplorador(self.ruta_verdura)).grid(column=2, row=5, padx=50)
 
 		#1.2 - Selección de algoritmos y vista previa
-		Label(self.frame_uno, text='Seleccionar algoritmo:', bg='white', fg= 'black', font= ('Arial', 13)).grid(column=1, row=6, pady=50)
-		self.radio1 = Radiobutton(self.frame_uno,text="KNN", variable=self.seleccion, value=1).grid(column=1, row=7)
-		self.radio2 = Radiobutton(self.frame_uno,text="Arbol", variable=self.seleccion, value=2).grid(column=1, row=8)
-		self.radio3 = Radiobutton(self.frame_uno,text="Varon", variable=self.seleccion, value=3).grid(column=1, row=9)
-		Label(self.frame_uno, text='Seleccionar algoritmo:', bg='white', fg= 'black', font= ('Arial', 13)).grid(column=1, row=6, pady=50)
+		self.radio1 = Radiobutton(self.frame_uno, justify=LEFT, command=self.seleccionaAlgoritmo("KNN"), text="KNN", variable=self.seleccion, value="KNN").grid(column=0, row=8)
+		self.radio2 = Radiobutton(self.frame_uno, justify=LEFT, command=self.seleccionaAlgoritmo("GBT"), text="Gradient Boosted Tree", variable=self.seleccion, value="GBT").grid(column=0, row=9)
+		self.radio3 = Radiobutton(self.frame_uno, justify=LEFT, command=self.seleccionaAlgoritmo("Random Forest"), text="Random Forest", variable=self.seleccion, value="Random Forest").grid(column=0, row=10)
 
-		Label(self.frame_uno, justify=LEFT, text='VISTA PREVIA:', bg='white', fg= 'black', font= ('Arial', 13, 'bold')).grid(column=1, row=10, pady=50)
-		Label(self.frame_uno, justify=LEFT, text='Aperitivos encontrados     - ' + str(self.aperitivos), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=11)
-		Label(self.frame_uno, justify=LEFT, text='Carnes encontradas         - ' + str(self.carnes) + '                                  Modelo Seleccionado', bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=12)
-		Label(self.frame_uno, justify=LEFT, text='Pastas encontradas          - ' + str(self.pastas) + '                                                ' + str(self.seleccion.get()), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=13)
-		Label(self.frame_uno, justify=LEFT, text='Pescados encontrados     - ' + str(self.pescados), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=14)
-		Label(self.frame_uno, justify=LEFT, text='Verduras encontrados      - ' + str(self.verduras), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=15)
-		Label(self.frame_uno, justify=LEFT, text='Total: ' + str(self.total), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=16, pady=10)
-		Button(self.frame_uno, width=12, text='ENTRENAR!').grid(column=2, row=13, padx=0)
+		Label(self.frame_uno, justify=LEFT, text='VISTA PREVIA:', bg='white', fg= 'black', font= ('Arial', 13, 'bold')).grid(column=1, row=6, pady=70)
+		Label(self.frame_uno, justify=LEFT, text='Aperitivos encontrados     - ' + str(self.aperitivos.get()), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=7)
+		Label(self.frame_uno, justify=LEFT, text='Carnes encontradas         - ' + str(self.carnes.get()) + '                                  Modelo Seleccionado:', bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=8)
+		Label(self.frame_uno, justify=LEFT, text='Pastas encontradas          - ' + str(self.pastas.get()) + '                                      ' + str(self.seleccion.get()), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=9)
+		Label(self.frame_uno, justify=LEFT, text='Pescados encontrados     - ' + str(self.pescados.get()), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=10)
+		Label(self.frame_uno, justify=LEFT, text='Verduras encontrados      - ' + str(self.verduras.get()), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=11)
+		Label(self.frame_uno, justify=LEFT, text='Total: ' + str(self.total.get()), bg='white', fg= 'black', font= ('Arial', 13)).grid(sticky = W, column=1, row=12, pady=10)
+		Button(self.frame_uno, width=12, text='ENTRENAR!', bg='red2', fg='white', font= ('Arial', 13, 'bold')).grid(column=2, row=9, padx=0)
 
 		#1.3 - Guardar modelo
 		Label(self.frame_uno, width=15, text='Guardar modelo:', bg='white', fg= 'black', font=('Arial', 13)).grid(column=0, row=17, pady=50)
@@ -129,21 +139,23 @@ class Ventana(Frame):
 		# Página 2 - Clasificación
 		Label(self.frame_dos, text= 'CLASIFICACIÓN', bg='white', fg= 'black', font= ('Arial', 15, 'bold')).grid(column=1, row=0, pady=12)
 		Label(self.frame_dos, width=15, text='Textos a clasificar:', bg='white', fg= 'black', font=('Arial', 13)).grid(column=0, row=1, pady=12, padx=100)
-		Entry(self.frame_dos, state= "disabled", width=80, text=self.ruta_otros, textvariable=self.ruta_otros, font=('Arial', 10), highlightbackground = "#061a2b", highlightthickness=3).grid(column=1,row=1, padx=140)
+		Entry(self.frame_dos, state= "disabled", width=80, text=self.ruta_otros, textvariable=self.ruta_otros, font=('Arial', 10), highlightbackground = "#061a2b", highlightthickness=3).grid(column=1,row=1, padx=115)
 		Button(self.frame_dos, width=12, text='Seleccionar').grid(column=2, row=1, padx=50)
 		Label(self.frame_dos, width=18, text='Modelo clasificador:', bg='white', fg= 'black', font=('Arial', 13)).grid(column=0, row=2, pady=12, padx=100)
-		Entry(self.frame_dos, state= "disabled", width=80, text=self.ruta_modelo, textvariable=self.ruta_modelo, font=('Arial', 10), highlightbackground = "#061a2b", highlightthickness=3).grid(column=1,row=2, padx=140)
+		Entry(self.frame_dos, state= "disabled", width=80, text=self.ruta_modelo, textvariable=self.ruta_modelo, font=('Arial', 10), highlightbackground = "#061a2b", highlightthickness=3).grid(column=1,row=2, padx=115)
 		Button(self.frame_dos, width=12, text='Seleccionar').grid(column=2, row=2, padx=50)
 
 		# Página 3 - Web Scraping
 		Label(self.frame_tres, text= 'WEB SCRAPING', bg='white', fg= 'black', font= ('Arial', 15, 'bold')).pack(expand=1)
 
 # Contar cuantos textos hay en una carpeta
-def cuentaTextos(ruta, tipo):
-	for ruta in pathlib.Path(".").iterdir():
-		if ruta.is_file():
-			tipo += 1
-	return tipo
+def contarTextos(folder):
+    count = 0
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(".txt"):
+                count += 1
+    return count
 
 # Settings ventana
 if __name__ == "__main__":
@@ -151,7 +163,6 @@ if __name__ == "__main__":
     ventana.title("Rady Recipes")
     ventana.resizable(True, True)
     ventana.attributes('-fullscreen',True)
-    ventana.call('wm', 'iconphoto', ventana._w, PhotoImage(file='./interfaz/readyrecipes_logo.png'))
 
     app = Ventana(ventana)
     ventana.mainloop()
