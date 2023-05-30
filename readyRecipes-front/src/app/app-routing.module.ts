98251c5bc4bc2e6fd93eb9f1817d5b00fbfaf738
Pablo González Sánchez <pgonzalezs1999@gmail.com>
2023-05-30 10:59:01 +0200

    Mas estilizado

--- readyRecipes-front/src/app/app-routing.module.ts
+++ readyRecipes-front/src/app/app-routing.module.ts
@@ -6,13 +6,11 @@ import { BibliotecaComponent } from './views/biblioteca/biblioteca.component';
 import { RecetaComponent } from './views/receta/receta.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegistroComponent } from './views/registro/registro.component';
-import { RecuperarComponent } from './views/recuperar/recuperar.component';
 import { EstadisticasComponent } from './views/estadisticas/estadisticas.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
-  {path: "recuperar", component: RecuperarComponent},
   {path: "registro", component: RegistroComponent},
   {path: "biblioteca", component: BibliotecaComponent},
   {path: "receta/:id", component: RecetaComponent},
